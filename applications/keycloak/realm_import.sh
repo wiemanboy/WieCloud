d869b0f7dde31562b621aba814f92cf87cfa2fd2
wiemanboy <wiemanboy@gmail.com>
2025-12-01 17:53:27 +0100

    ADD: oidc

---
+++ applications/keycloak/realm_import.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+REALM_NAME=master
+TARGET_DIR=./master-realm.temp
+NAMESPACE=keycloak
+SECRET_NAME=keycloak-${REALM_NAME}-realm
+
+mkdir -p "$TARGET_DIR"
+
+kubectl get secret "$SECRET_NAME" -n "$NAMESPACE" -o jsonpath='{.data.master-realm\.json}' | base64 -d > "$TARGET_DIR/$REALM_NAME-realm.import.json"
