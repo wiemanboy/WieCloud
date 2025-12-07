6512c882a5d1d48dd5194ac5c9c2481cf56dbc86
wiemanboy <wiemanboy@gmail.com>
2025-12-07 19:06:34 +0100

    TEST: secret from secret

--- _wiecloud_setup.sh
+++ _wiecloud_setup.sh
@@ -47,6 +47,7 @@ kubectl create secret generic keycloak-db-secret -n keycloak --from-literal=user
 
 HARBOR_SECRET=$(yq '.wiecloud.harbor.oidc.clientSecret.secret' env.yaml --raw-output)
 kubectl create secret generic harbor-client-secret -n harbor --from-literal=secret=$HARBOR_SECRET
+kubectl create secret generic harbor-client-secret -n keycloak --from-literal=secret=$HARBOR_SECRET
 
 CLOUDFLARE_API_TOKEN=$(yq '.wiecloud.cloudflare.apiToken' env.yaml --raw-output)
 kubectl create secret generic cloudflare-api-token --from-literal=api-token=$CLOUDFLARE_API_TOKEN \--namespace=gateway
