e5b236150e46d52c7085524f28b8466cee82930c
wiemanboy <94828396+wiemanboy@users.noreply.github.com>
2025-12-24 18:03:07 +0100

    Manage wiecloud applications (#6)

--- foundation/cluster/crd.tf
+++ foundation/cluster/crd.tf
@@ -6,10 +6,6 @@ data "http" "keycloak_realm_crd" {
   url = "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.4.7/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml"
 }
 
-data "http" "keycloak_operator" {
-  url = "https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.4.7/kubernetes/kubernetes.yml"
-}
-
 resource "kubernetes_manifest" "keycloak_crd" {
   manifest = yamldecode(data.http.keycloak_crd.response_body)
 }
@@ -18,6 +14,3 @@ resource "kubernetes_manifest" "keycloak_realm_crd" {
   manifest = yamldecode(data.http.keycloak_realm_crd.response_body)
 }
 
-# resource "kubernetes_manifest" "keycloak_operator" {
-#     manifest = yamldecode(data.http.keycloak_operator.response_body)
-# }
