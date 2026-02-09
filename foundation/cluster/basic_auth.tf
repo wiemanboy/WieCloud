47be852a1ad7fa2295177efa582d0987a61b37bd
wiemanboy <wiemanboy@gmail.com>
2026-02-09 22:38:10 +0100

    UPDATE: remove ns from tfstate

---
+++ foundation/cluster/basic_auth.tf
@@ -0,0 +1,16 @@
+locals {
+  basic_auth_namespaces = ["gateway", "longhorn-system"]
+}
+
+resource "kubernetes_secret_v1" "server-auth-secret" {
+  for_each = { for ns in local.basic_auth_namespaces : ns => {} }
+  type     = "kubernetes.io/basic-auth"
+  metadata {
+    name      = "server-auth-secret"
+    namespace = each.key
+  }
+  data = {
+    username = "admin",
+    password = local.env.wiecloud.basic-auth.password
+  }
+}
