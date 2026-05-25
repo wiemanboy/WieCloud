4348c0627556d9f34b3dbcda5076f0dda82f6673
wiemanboy <wiemanboy@gmail.com>
2026-05-25 21:36:20 +0200

    UPDATE: deploy keycloak via deployment

--- helper/setup/setup.sh
+++ helper/setup/setup.sh
@@ -12,9 +12,5 @@ kubectl apply -f argo/wiecloud.application.yaml
 
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/experimental-install.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.5.2/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.5.2/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
-kubectl -n keycloak apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.5.2/kubernetes/kubernetes.yml
-
 DEFAULT_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo "default password: $DEFAULT_PASSWORD"
