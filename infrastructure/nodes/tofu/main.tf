55a32d4fcfce8a4a4c4ee655e27c09df103595a0
wiemanboy <wiemanboy@gmail.com>
2026-06-02 17:15:23 +0200

    ADD: tfstate backend

--- infrastructure/nodes/tofu/main.tf
+++ infrastructure/nodes/tofu/main.tf
@@ -1,8 +1,16 @@
+terraform {
+  backend "local" {
+    path = "/data/state/terraform.tfstate"
+  }
+}
+
 provider "proxmox" {
   pm_api_url          = var.proxmox_api_url
   pm_api_token_id     = var.proxmox_user_id
   pm_api_token_secret = var.proxmox_user_secret
   pm_tls_insecure     = true
 }
+
+provider "kubernetes" {}
 
 provider "talos" {}
