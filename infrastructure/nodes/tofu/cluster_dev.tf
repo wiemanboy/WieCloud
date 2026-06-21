6ed35c03ffb4bc928852ebe1b6788ab057fb74a4
wiemanboy <wiemanboy@gmail.com>
2026-06-21 12:35:53 +0200

    UPDATE: destory dev cluster

--- infrastructure/nodes/tofu/cluster_dev.tf
+++ infrastructure/nodes/tofu/cluster_dev.tf
@@ -17,80 +17,80 @@ locals {
   }
 }
 
-module "dev_talos_image" {
-  source        = "./modules/talos/image_factory"
-  talos_version = local.dev_talos_version
-}
+# module "dev_talos_image" {
+#   source        = "./modules/talos/image_factory"
+#   talos_version = local.dev_talos_version
+# }
 
-resource "proxmox_storage_iso" "dell_pve_0_metal_amd64_iso" {
-  pve_node = "dell-pve-0"
-  storage  = "local"
-  filename = "metal-amd64.iso"
-  url      = "https://factory.talos.dev/image/${module.dev_talos_image.id}/${module.dev_talos_image.talos_version}/metal-amd64.iso"
-}
+# resource "proxmox_storage_iso" "dell_pve_0_metal_amd64_iso" {
+#   pve_node = "dell-pve-0"
+#   storage  = "local"
+#   filename = "metal-amd64.iso"
+#   url      = "https://factory.talos.dev/image/${module.dev_talos_image.id}/${module.dev_talos_image.talos_version}/metal-amd64.iso"
+# }
 
-resource "talos_machine_secrets" "dev_machine_secret" {
-  talos_version = local.dev_talos_version
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
-}
+# resource "talos_machine_secrets" "dev_machine_secret" {
+#   talos_version = local.dev_talos_version
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-module "dev_talos_controlplane_0" {
-  source   = "./modules/node"
-  name     = "talos-controlplane-0"
-  node     = "dell-pve-0"
-  region   = "nl-central-0"
-  endpoint = module.dev_talos_controlplane_0.ip
-  cluster  = local.dev_cluster
-  role     = "controlplane"
+# module "dev_talos_controlplane_0" {
+#   source   = "./modules/node"
+#   name     = "talos-controlplane-0"
+#   node     = "dell-pve-0"
+#   region   = "nl-central-0"
+#   endpoint = module.dev_talos_controlplane_0.ip
+#   cluster  = local.dev_cluster
+#   role     = "controlplane"
 
-  ip      = "192.168.178.210"
-  macaddr = "AA:00:FF:E2:AF:FD"
+#   ip      = "192.168.178.210"
+#   macaddr = "AA:00:FF:E2:AF:FD"
 
-  machine_secret = talos_machine_secrets.dev_machine_secret
-  talos_version  = local.dev_talos_version
-  iso            = local.dell_pve_0_metal_amd64_iso
-  image          = local.dev_image
-  bootstrap      = true
+#   machine_secret = talos_machine_secrets.dev_machine_secret
+#   talos_version  = local.dev_talos_version
+#   iso            = local.dell_pve_0_metal_amd64_iso
+#   image          = local.dev_image
+#   bootstrap      = true
 
-  spec = {
-    cpu_cores = 6
-    disk_size = 50
-    memory    = 8192
-  }
+#   spec = {
+#     cpu_cores = 6
+#     disk_size = 50
+#     memory    = 8192
+#   }
 
-  oidc = {
-    issuer_url = var.oidc_issuer_url
-  }
+#   oidc = {
+#     issuer_url = var.oidc_issuer_url
+#   }
 
-  extra_config = local.dev_cilium_config
-}
+#   extra_config = local.dev_cilium_config
+# }
 
-module "dev_talos_worker_0" {
-  depends_on = [module.dev_talos_controlplane_0]
-  source     = "./modules/node"
+# module "dev_talos_worker_0" {
+#   depends_on = [module.dev_talos_controlplane_0]
+#   source     = "./modules/node"
 
-  name     = "talos-worker-0"
-  node     = "dell-pve-0"
-  region   = "nl-central-0"
-  endpoint = module.dev_talos_controlplane_0.ip
-  cluster  = local.dev_cluster
-  role     = "worker"
+#   name     = "talos-worker-0"
+#   node     = "dell-pve-0"
+#   region   = "nl-central-0"
+#   endpoint = module.dev_talos_controlplane_0.ip
+#   cluster  = local.dev_cluster
+#   role     = "worker"
 
-  ip      = "192.168.178.211"
-  macaddr = "AA:00:FF:85:16:26"
+#   ip      = "192.168.178.211"
+#   macaddr = "AA:00:FF:85:16:26"
 
-  machine_secret = talos_machine_secrets.dev_machine_secret
-  talos_version  = local.dev_talos_version
-  iso            = local.dell_pve_0_metal_amd64_iso
-  image          = local.dev_image
+#   machine_secret = talos_machine_secrets.dev_machine_secret
+#   talos_version  = local.dev_talos_version
+#   iso            = local.dell_pve_0_metal_amd64_iso
+#   image          = local.dev_image
 
-  spec = {
-    cpu_cores = 8
-    disk_size = 100
-    memory    = 16384
-  }
+#   spec = {
+#     cpu_cores = 8
+#     disk_size = 100
+#     memory    = 16384
+#   }
 
-  extra_config = local.dev_cilium_config
-}
+#   extra_config = local.dev_cilium_config
+# }
