53364a556302aaca37389b7ad5af180833dac987
wiemanboy <wiemanboy@gmail.com>
2026-08-20 21:46:05 +0200

    FIX: command

--- products/forgejorunnercontroller/image/src/runner/runner.go
+++ products/forgejorunnercontroller/image/src/runner/runner.go
@@ -72,18 +72,20 @@ func createJob(secret string, forgejoNamespace string, runnerNamespace string, f
 
 func job(secret string, forgejoNamespace string, forgejoImage string, kubectlImage string, podYaml string) *batchv1.Job {
 	registerCmd := `
-	forgejo forgejo-cli actions register \
-		--name "%s"\
-		--secret "%s" \
-		--ephemeral \
-		> /shared/uuid 2>&1
+forgejo forgejo-cli actions register \
+	--name "%s"\
+	--secret "%s" \
+	--ephemeral \
+	> /shared/uuid 2>&1
 	`
 
 	createRunnerCmd := `
-	UUID=$(/shared/uuid)
-	NAME=%s-${UUID}
+UUID=$(/shared/uuid)
+NAME=%s-${UUID}
 
-	echo "%s" | kubectl apply -f -
+kubectl apply -f - <<EOF
+%s
+EOF
 	`
 
 	return &batchv1.Job{
@@ -151,27 +153,27 @@ func job(secret string, forgejoNamespace string, forgejoImage string, kubectlIma
 
 func pod(secret string, forgejoInstance string, namespace string, forgejoImage string, dindImage string) *v1.Pod {
 	runnerCmd := `
-	cp /tmp/runner/config.yaml /etc/runner/config.yaml
+cp /tmp/runner/config.yaml /etc/runner/config.yaml
 
-	awk -v name="%s" -v url="%s" -v uuid="$RUNNER_UUID" -v token="$RUNNER_SECRET" '
-	/^  connections:/ && !done {
-		print $0
-		print "    " name ":"
-		print "      url: " url
-		print "      uuid: " uuid
-		print "      token: " token
-		done=1
-		next
-	}
-	done && /^  [^ ]/ {
-		done=0
-	}
-	done {
-		next
-	}
-	1' /etc/runner/config.yaml > /etc/runner/config.yaml.tmp && mv /etc/runner/config.yaml.tmp /etc/runner/config.yaml
+awk -v name="%s" -v url="%s" -v uuid="$RUNNER_UUID" -v token="$RUNNER_SECRET" '
+/^  connections:/ && !done {
+	print $0
+	print "    " name ":"
+	print "      url: " url
+	print "      uuid: " uuid
+	print "      token: " token
+	done=1
+	next
+}
+done && /^  [^ ]/ {
+	done=0
+}
+done {
+	next
+}
+1' /etc/runner/config.yaml > /etc/runner/config.yaml.tmp && mv /etc/runner/config.yaml.tmp /etc/runner/config.yaml
 
-	/bin/forgejo-runner --config /etc/runner/config.yaml daemon
+/bin/forgejo-runner --config /etc/runner/config.yaml daemon
 	`
 
 	return &v1.Pod{
