76a2f4c3047f6bc91ba41141b70b2ecc95dfe6d2
wiemanboy <wiemanboy@gmail.com>
2026-08-20 16:39:26 +0000

    ADD: port forward and vlan config

---
+++ infrastructure/nodes/tofu/test/port_forward.tf
@@ -0,0 +1,17 @@
+resource "routeros_ip_firewall_nat" "http" {
+  comment  = "HTTP"
+  chain    = "dstnat"
+  action   = "dst-nat"
+  protocol = "tcp"
+  src_port = 8080
+  dst_port = 80
+}
+
+resource "routeros_ip_firewall_nat" "https" {
+  comment  = "HTTPS"
+  chain    = "dstnat"
+  action   = "dst-nat"
+  protocol = "tcp"
+  src_port = 8443
+  dst_port = 443
+}
--- infrastructure/nodes/tofu/test/vlan.tf
+++ infrastructure/nodes/tofu/test/vlan.tf
@@ -1,5 +1,10 @@
-resource "routeros_interface_vlan" "interface_vlan" {
-  interface = ""
-  name      = "VLAN_TEST"
-  vlan_id   = 50
+resource "routeros_interface_vlan" "wiecloud_vlan" {
+  interface = "ether2"
+  name      = "WIECLOUD_VLAN"
+  vlan_id   = 1
+}
+
+resource "routeros_ip_address" "ip_address" {
+  address   = "10.0.0.1/24"
+  interface = routeros_interface_vlan.wiecloud_vlan.name
 }
