a2c1fcdd55690fc0e9bce747ddd1c50850290e03
wiemanboy <wiemanboy@gmail.com>
2026-08-20 19:33:57 +0200

    FIX: runners count

--- products/forgejorunnercontroller/image/src/runner/runner.go
+++ products/forgejorunnercontroller/image/src/runner/runner.go
@@ -8,7 +8,6 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-  "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/utils/ptr"
 )
@@ -32,19 +31,15 @@ func Create(amount int, secret string, namespace string, client *kubernetes.Clie
 func count(namespace string, client *kubernetes.Clientset) (int, error) {
 	log.Println("Counting runners")
 
-  labelSelector := metav1.LabelSelector{MatchLabels: map[string]string{runnerLabel:""}}
-
-	log.Println("Label selector:", labels.Set(labelSelector.MatchLabels).String())
-
-	pods, err := client.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: labels.Set(labelSelector.MatchLabels).String()})
-	jobs, err := client.BatchV1().Jobs(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: labels.Set(labelSelector.MatchLabels).String()})
+	pods, err := client.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: runnerLabel})
+	jobs, err := client.BatchV1().Jobs(namespace).List(context.Background(), metav1.ListOptions{LabelSelector: runnerLabel})
 
 	if err != nil {
 		log.Println("Failed to list jobs")
 		return 0, err
 	}
 
-	return pods.Size() + jobs.Size(), nil
+	return len(pods.Items) + len(jobs.Items), nil
 }
 
 func createJob(secret string, namespace string, client *kubernetes.Clientset) {
