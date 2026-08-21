866c0cb381c0017e928b6d2be07d41d275307168
wiemanboy <wiemanboy@gmail.com>
2026-08-21 22:41:14 +0200

    FIX: register command

--- products/forgejorunnercontroller/image/src/runner/runner.go
+++ products/forgejorunnercontroller/image/src/runner/runner.go
@@ -74,8 +74,7 @@ func job(secret string, forgejoNamespace string, forgejoImage string, kubectlIma
 forgejo forgejo-cli actions register \
   --name "%s" \
   --secret "%s" \
-  --ephemeral
-	> /shared/uuid 2>&1
+  > /shared/uuid 2>&1
 `, runnerName, secret)
 
 	createRunnerCmd := fmt.Sprintf(`
