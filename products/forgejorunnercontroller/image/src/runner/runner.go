bc6ac1974390d423175ca63dce2f7a0d4e49641b
wiemanboy <wiemanboy@gmail.com>
2026-08-22 17:25:54 +0200

    UPDATE: make pod a deployment

--- products/forgejorunnercontroller/image/src/runner/runner.go
+++ products/forgejorunnercontroller/image/src/runner/runner.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
+
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -55,9 +57,9 @@ func Create(amount int, secretName string, register Register, runner Runner, cli
 func createJob(secretRefs secret.SecretsRefs, register Register, runner Runner, client *kubernetes.Clientset) {
 	log.Println("Creating job")
 
-	runnerPod := pod(secretRefs, runner)
+	runnerDeployment := deployment(secretRefs, runner)
 
-	podBytes, _ := yaml.Marshal(runnerPod)
+	podBytes, _ := yaml.Marshal(runnerDeployment)
 	podYaml := string(podBytes)
 
 	_, err := client.BatchV1().Jobs(register.Namespace).Create(
@@ -182,7 +184,7 @@ EOF
 	}
 }
 
-func pod(secretRefs secret.SecretsRefs, runner Runner) *corev1.Pod {
+func deployment(secretRefs secret.SecretsRefs, runner Runner) *appsv1.Deployment {
 	// Below command needs to be bash escaped
 	runnerCmd := fmt.Sprintf(`
 cp /tmp/runner/config.yaml /etc/runner/config.yaml
@@ -215,15 +217,18 @@ done
 /bin/forgejo-runner --config /etc/runner/config.yaml daemon
 `, runner.Name, runner.Instance)
 
-	return &corev1.Pod{
+	return &appsv1.Deployment{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
-			Kind:       "Pod",
+			Kind:       "Deployment",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:        "${NAME}",
-			Namespace:   runner.Namespace,
-			Labels:      map[string]string{runner.Label: ""},
+			Name:      "${NAME}",
+			Namespace: runner.Namespace,
+			Labels: map[string]string{
+				runner.Label: "",
+				"app":        runner.Name,
+			},
 			Annotations: map[string]string{"argocd.argoproj.io/tracking-id": "forgejo:apps/Deployment:forgejo/forgejo-runner-controller"},
 			OwnerReferences: []metav1.OwnerReference{{
 				Name:               secretRefs.RunnerSecret.Name,
@@ -234,79 +239,98 @@ done
 				Kind:               secretRefs.RunnerSecret.Kind,
 			}},
 		},
-		Spec: corev1.PodSpec{
-			InitContainers: []corev1.Container{{
-				Name:          "dind",
-				Image:         runner.DindImage,
-				RestartPolicy: ptr.To(corev1.ContainerRestartPolicyAlways),
-				Command: []string{
-					"dockerd",
-					"-H",
-					"tcp://0.0.0.0:2375",
-					"--tls=false",
+		Spec: appsv1.DeploymentSpec{
+			Replicas: ptr.To(int32(1)),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": runner.Name,
 				},
-				SecurityContext: &corev1.SecurityContext{
-					Privileged: ptr.To(true),
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						runner.Label: "",
+						"app":        runner.Name,
+					},
+					Annotations: map[string]string{
+						"wieman.cloud/runner-config-checksum": runner.ConfigChecksum,
+					},
 				},
-			}},
+				Spec: corev1.PodSpec{
+					InitContainers: []corev1.Container{{
+						Name:          "dind",
+						Image:         runner.DindImage,
+						RestartPolicy: ptr.To(corev1.ContainerRestartPolicyAlways),
+						Command: []string{
+							"dockerd",
+							"-H",
+							"tcp://0.0.0.0:2375",
+							"--tls=false",
+						},
+						SecurityContext: &corev1.SecurityContext{
+							Privileged: ptr.To(true),
+						},
+					}},
 
-			Containers: []corev1.Container{{
-				Name:  "forgejo-runner",
-				Image: runner.Image,
-				Env: []corev1.EnvVar{
-					{
-						Name: "RUNNER_SECRET",
-						ValueFrom: &corev1.EnvVarSource{
-							SecretKeyRef: &corev1.SecretKeySelector{
-								LocalObjectReference: corev1.LocalObjectReference{Name: secretRefs.RunnerSecret.Name},
-								Key:                  "secret",
+					Containers: []corev1.Container{{
+						Name:  "forgejo-runner",
+						Image: runner.Image,
+						Env: []corev1.EnvVar{
+							{
+								Name: "RUNNER_SECRET",
+								ValueFrom: &corev1.EnvVarSource{
+									SecretKeyRef: &corev1.SecretKeySelector{
+										LocalObjectReference: corev1.LocalObjectReference{Name: secretRefs.RunnerSecret.Name},
+										Key:                  "secret",
+									},
+								},
+							},
+							{
+								Name:  "RUNNER_UUID",
+								Value: "${UUID}",
 							},
 						},
-					},
-					{
-						Name:  "RUNNER_UUID",
-						Value: "${UUID}",
-					},
-				},
-				Command: []string{
-					"sh",
-					"-c",
-					runnerCmd,
-				},
-				SecurityContext: &corev1.SecurityContext{
-					Privileged: ptr.To(true),
-				},
-				VolumeMounts: []corev1.VolumeMount{
-					{
-						Name:      "runner",
-						MountPath: "/etc/runner",
-					},
-					{
-						Name:      "runner-data",
-						MountPath: "/data",
-					},
-					{
-						Name:      "runner-config",
-						MountPath: "/tmp/runner",
-					},
-				},
-			}},
+						Command: []string{
+							"sh",
+							"-c",
+							runnerCmd,
+						},
+						SecurityContext: &corev1.SecurityContext{
+							Privileged: ptr.To(true),
+						},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      "runner",
+								MountPath: "/etc/runner",
+							},
+							{
+								Name:      "runner-data",
+								MountPath: "/data",
+							},
+							{
+								Name:      "runner-config",
+								MountPath: "/tmp/runner",
+							},
+						},
+					}},
 
-			Volumes: []corev1.Volume{
-				{
-					Name:         "runner",
-					VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
-				},
-				{
-					Name:         "runner-data",
-					VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
-				},
-				{
-					Name: "runner-config",
-					VolumeSource: corev1.VolumeSource{
-						ConfigMap: &corev1.ConfigMapVolumeSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: "forgejo-runner-config",
+					Volumes: []corev1.Volume{
+						{
+							Name:         "runner",
+							VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+						},
+						{
+							Name:         "runner-data",
+							VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+						},
+						{
+							Name: "runner-config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "forgejo-runner-config",
+									},
+								},
 							},
 						},
 					},
