d5d7790fbacafdf1ccd02d5c8c9b6125bed53881
wiemanboy <wiemanboy@gmail.com>
2026-09-12 16:05:03 +0200

    ADD: proper vlan configuration

--- infrastructure/nodes/tofu/test/main.tf
+++ infrastructure/nodes/tofu/test/main.tf
@@ -1,9 +1,9 @@
 provider "routeros" {
-  hosturl  = "http://192.168.88.1"
+  hosturl  = "http://10.0.0.1"
   username = "admin"
   password = var.routeros_password
 }
 
-resource "routeros_system_identity" "identity" {
-  name = "WieCloudRouter"
+resource "routeros_system_identity" "wiecloud_router" {
+  name = "WieCloud_Router"
 }
--- infrastructure/nodes/tofu/test/vlan.tf
+++ infrastructure/nodes/tofu/test/vlan.tf
@@ -1,66 +1,66 @@
-resource "routeros_interface_bridge" "bridge1" {
-  name           = "bridge1"
+resource "routeros_interface_bridge" "wiecloud_bridge" {
+  comment        = "wiecloud"
+  name           = "wiecloud-bridge"
   frame_types    = "admit-only-vlan-tagged"
   vlan_filtering = true
 }
 
-resource "routeros_interface_bridge_port" "bridge1_ether2" {
-  bridge      = "bridge1"
+resource "routeros_interface_bridge_port" "wiecloud_bridge_ether2" {
+  comment     = "wiecloud"
+  bridge      = routeros_interface_bridge.wiecloud_bridge.name
   interface   = "ether2"
-  frame_types = "admit-only-vlan-tagged"
-}
-
-resource "routeros_interface_bridge_port" "bridge1_ether3" {
-  bridge      = "bridge1"
-  interface   = "ether3"
   frame_types = "admit-only-untagged-and-priority-tagged"
-  pvid        = 30
+  pvid        = 10
 }
 
-resource "routeros_interface_bridge_port" "bridge1_ether4" {
-  bridge      = "bridge1"
-  interface   = "ether4"
-  frame_types = "admit-only-untagged-and-priority-tagged"
-  pvid        = 40
+resource "routeros_interface_bridge_vlan" "wiecloud_bridge_vlan" {
+  comment  = "wiecloud"
+  bridge   = routeros_interface_bridge.wiecloud_bridge.name
+  vlan_ids = ["10"]
+  untagged = [routeros_interface_bridge_port.wiecloud_bridge_ether2.interface]
+  tagged   = [routeros_interface_bridge.wiecloud_bridge.name]
 }
 
-resource "routeros_bridge_vlan" "vlan_30" {
-  bridge   = "bridge1"
-  tagged   = ["ether2"]
-  vlan_ids = [30]
+resource "routeros_interface_vlan" "wiecloud_vlan" {
+  comment   = "wiecloud"
+  name      = "wiecloud-vlan"
+  interface = routeros_interface_bridge.wiecloud_bridge.name
+  vlan_id   = 10
 }
 
-resource "routeros_bridge_vlan" "vlan_40" {
-  bridge   = "bridge1"
-  tagged   = ["ether2"]
-  vlan_ids = [40]
+resource "routeros_ip_address" "wiecloud_gateway_ip" {
+  comment   = "wiecloud"
+  address   = "10.0.0.1/24"
+  interface = routeros_interface_vlan.wiecloud_vlan.name
 }
 
-resource "routeros_bridge_vlan" "vlan_99" {
-  bridge   = "bridge1"
-  tagged   = ["ether2", "bridge1"]
-  vlan_ids = [99]
+resource "routeros_ip_pool" "wiecloud_ip_pool" {
+  comment = "wiecloud"
+  name    = "wiecloud-ip-pool"
+  ranges  = ["10.0.0.100-10.0.0.200"]
 }
 
-resource "routeros_interface_vlan" "vlan_99" {
-  name      = "MGMT"
-  interface = "bridge1"
-  vlan_id   = 99
-
+resource "routeros_ip_dhcp_server" "wiecloud_dhcp_server" {
+  name         = "wiecloud_dhcp"
+  address_pool = routeros_ip_pool.wiecloud_ip_pool.name
+  interface    = routeros_interface_vlan.wiecloud_vlan.name
 }
 
-resource "routeros_ip_address" "ip_address" {
-  address   = "192.168.99.1/24"
-  interface = "MGMT"
+resource "routeros_interface_list" "vlan" {
+  name    = "VLAN"
 }
 
-resource "routeros_interface_vlan" "vlan30" {
-  name      = "VLAN30"
-  interface = "bridge1"
-  vlan_id   = 30
+resource "routeros_interface_list_member" "list_member" {
+  comment   = "wiecloud"
+  list      = routeros_interface_list.vlan.name
+  interface = routeros_interface_vlan.wiecloud_vlan.name
 }
 
-resource "routeros_ip_address" "vlan30" {
-  address   = "192.168.30.1/24"
-  interface = "VLAN30"
+resource "routeros_ip_firewall_filter" "rule" {
+  comment           = "wiecloud: Allow all coming from VLAN"
+  in_interface_list = routeros_interface_list.vlan.name
+  place_before      = 0
+  action            = "accept"
+  chain             = "input"
+  // src_address       = "10.0.0.200" // ip of the management pod
 }
